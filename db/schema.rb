c242e613827f7a58106db7920003aaaa6c6febd5
Rohan Dhalpe <rohandhalpe05@gmail.com>
2024-01-30 14:21:39 +0530

    validations and other migrations like rename, add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_29_112450) do
+ActiveRecord::Schema.define(version: 2024_01_30_084750) do
 
   create_table "book_issues", force: :cascade do |t|
     t.integer "issue_id"
@@ -46,9 +46,10 @@ ActiveRecord::Schema.define(version: 2024_01_29_112450) do
     t.string "first_name"
     t.string "last_name"
     t.string "city"
-    t.string "mobile_no"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
