cd45cb787fd30dfd94a5274b1647f550001703b8
Rohan Dhalpe <rohandhalpe05@gmail.com>
2024-01-31 12:50:50 +0530

    Added changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
-  after_create -> { puts "Congratulations!" }
+  after_create -> { puts 'Congratulations!' }
 end
