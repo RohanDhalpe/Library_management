7f9361f7f25acf8b2ce2277fe2086b87e52a9580
Rohan Dhalpe <rohandhalpe05@gmail.com>
2024-02-01 16:58:16 +0530

    Learned more validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
+  validates :email, uniqueness: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :gender, inclusion: { in: %w(male female other) }
+  validates :password, length: { minimum: 8 }
+
   after_create -> { puts 'Congratulations!' }
 end
