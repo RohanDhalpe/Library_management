e2619679d0e3f623b27e0c29b6559fa966f79bc6
Rohan Dhalpe <rohandhalpe05@gmail.com>
2024-02-16 21:39:18 +0530

    view practise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,69 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
-    render json: @users
   end
 
   def show
-    if @user = User.find_by(id: params[:id])
-      render json: @user
-    else
-      render json: I18n.t('users.not_found')
-    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
   end
 
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: I18n.t('users.created_successfully')
-    else
-      render json: I18n.t('users.creation_failed')
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: t('success.messages.user.create') }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, alert: t('errors.messages.user.create') }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  def new
-    @user = User.new
-    render json: 'In new method'
-  end
-
   def update
-    @user = User.find_by(id: params[:id])
-
-    if @user&.update(user_params)
-      render json: @user
-    elsif @user
-      render json: I18n.t('users.update_unsuccessful')
-    else
-      render json: I18n.t('users.not_found')
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: t('success.messages.user.update') }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, alert: t('errors.messages.user.update') }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    @user = User.find_by(id: params[:id])
-
-    if @user&.destroy
-      render json: I18n.t('users.deleted_successfully')
-    else
-      render json: I18n.t('users.not_found')
+    @user.destroy
+    respond_to do |format|
+      if @user.destroyed?
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to users_url, alert: 'Failed to destroy user.' }
+        format.json { render json: { error: 'Failed to destroy user.' }, status: :unprocessable_entity }
+      end
     end
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = t('errors.messages.user.not_found')
+    redirect_to users_path
+  end
+
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, :last_name, :city, :phone_number, :email)
   end
 end
